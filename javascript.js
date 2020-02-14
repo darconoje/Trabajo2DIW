6ba0614adaea0f5c5a48a8c0ff403a3603756e5b
darconoje <45296856+darconoje@users.noreply.github.com>
2020-02-14 17:24:03 +0100

    Actualizado

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
-$( document ).ready(function() {
-    $("#topsPlataforma").hide();
+$("#topsPlataforma").show();
+$("#topsGenero").hide();
+
+$("#porGenero").click(function() {
+   $("#topsPlataforma").hide();
+   $("#topsGenero").show();
+});
+
+$("#porPlataforma").click(function() {
+	$("#topsPlataforma").show();
+	$("#topsGenero").hide();
 });
